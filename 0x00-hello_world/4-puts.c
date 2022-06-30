4e3937b92fbc85fce9fae6920309771d4ea70cd3
dvmabeka@gmail.com <dvmabeka@gmail.com>
2022-06-30 13:43:49 -0700

    printf funtion program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
+ * main-entry point
  *
  * Return: Always 0 (Succes)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/**
+ * main-Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	printf("%s", "with proper grammer, but the outcome is a piece of art,);
+
+	return (0);
+}	
