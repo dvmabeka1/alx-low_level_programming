e9519067dc4060bbc97b53700b3dd92189a4d853
dvmabeka@gmail.com <dvmabeka@gmail.com>
2022-07-06 16:43:01 -0700

    correction

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,43 +1,30 @@
 #include "main.h"
+
 /**
-
 * print_alphabet_x10 - prints alphabet 10 times lowercase
-
 *
-
 * Return: Always 0 (Success)
-
 */
 
 
 
 void print_alphabet_x10(void)
-
 {
-
-
-
 char alpha;
-
 int x;
-
-
-
 for (x = 1; x <= 10; x++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
 
 {
 
-	for (alpha = 'a'; alpha <= 'z'; alpha++)
+_putchar(alpha);
 
-	{
-
-		_putchar(alpha);
-
-	}
+}
 
 
 
-	_putchar('\n');
+_putchar('\n');
 
 }
 
