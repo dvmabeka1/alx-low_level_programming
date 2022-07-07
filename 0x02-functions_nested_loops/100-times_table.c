e7f354ec5c49d62108be1e59de62a6d3d9790f25
dvmabeka@gmail.com <dvmabeka@gmail.com>
2022-07-06 18:13:54 -0700

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,83 +7,42 @@
 */
 
 void print_times_table(int n)
-
 {
-
 int factor1, factor2, product, product1, product10, product100;
-
 if (!(n > 15 || n < 0))
-
 {
-
 for (factor1 = 0; factor1 <= n; factor1++)
-
 {
-
 	for (factor2 = 0; factor2 <= n; factor2++)
-
 	{
-
 	product = factor1 * factor2;
-
 		if (product >= 100)
-
 		{
-
 			_putchar(',');
-
 			_putchar(' ');
-
-
-
 		} else if (product >= 10)
-
 		{
-
 			_putchar(',');
-
 			_putchar(' ');
-
 			_putchar(' ');
-
 		} else
-
 		{
-
 			if (factor2 != 0)
-
 			{
-
 				_putchar(',');
-
 				_putchar(' ');
-
 				_putchar(' ');
-
 				_putchar(' ');
-
 	}}	product100 = product / 100;
-
 		product10 = (product / 10) % 10;
-
 		product1 = product % 10;
-
 		if (product100 != 0)
-
 		{
-
 			_putchar(product100 + 48);
-
 		}
-
 		if (product100 != 0 || product10 != 0)
-
 		{
-
 			_putchar(product10 + '0');
-
 		}
-
 			_putchar(product1 + '0');
-
 		} _putchar('\n'); }}}
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,54 +7,26 @@
  * @n: int
  * return 0
  */
-
-
-
 void print_to_98(int n)
-
 {
-
 int x;
-
 	if (n < 98)
-
 	{
-
 		for (x = n; x < 98; x++)
-
 		{
-
 			printf("%d, ", x);
-
-
-
 		}
-
-
-
 	}
-
-
 
 	else
 
 	{
-
 		for (x = n; x > 98; x--)
 
 		{
 
 			printf("%d, ", x);
-
-
-
 		}
-
-
-
-
-
-
 
 	}
 
